fe72bbc557ca93621d3a1f5b98bb317bef0a31f1
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-26 11:58:35 +0200

    initial commit

--- src/main/scala/WordCountPerLineApp.scala
+++ src/main/scala/WordCountPerLineApp.scala
@@ -1,3 +1,5 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
+
 object WordCountPerLineApp extends App {
 
   import org.apache.kafka.common.serialization.Serdes
@@ -14,22 +16,28 @@ object WordCountPerLineApp extends App {
   import ImplicitConversions._
   import serialization.Serdes._
   import org.apache.kafka.streams.scala.kstream._
-
+  import org.apache.kafka.streams.scala.ImplicitConversions._
+  import org.apache.kafka.streams.scala.Serdes._
+  import org.apache.kafka.streams.scala.StreamsBuilder
+  import org.apache.kafka.streams.scala.kstream._
 
   val props = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordCountPerLineAPP")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordCountPerLineAPPP")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
   props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
-  //props.put(StreamsConfig.)
+  props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+
   val builder = new StreamsBuilder
   builder
-    .stream[String, String]("inputTopic")
-    .flatMapValues { _.split("\\W+") }
+    .stream[String, String]("hello")
+    .flatMapValues {_.split("\\W+")}
     .groupBy { case (_, value) => value }
     .count
+    //opcjonalnie
+    .mapValues(_.toString)
     .toStream
-    .to("outputTopic")
+    .to("bye")
 
   val topology = builder.build
   val streams = new KafkaStreams(topology, props)
